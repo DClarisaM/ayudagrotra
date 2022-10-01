bc9560e1ef80bb5744e6ff22897ca3a010522575
yenifernataly2003 <yenifernataly2003@gmail.com>
2022-10-01 14:47:52 -0500

    yeni

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Inicio from '../views/InicioView.vue'
+import Registrar from '../views/RegistrarView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/Inicio',
+    name: 'Inicio',
+    component: Inicio
+  },
+  {
+    path: '/Registrar',
+    name: 'Registrar',
+    component: Registrar
   },
   {
     path: '/about',
